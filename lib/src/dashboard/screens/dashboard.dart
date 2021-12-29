e664f875ed2a30b810a5730d52ec4d77c0c3b414
brahmikumar <ajayit786@gmail.com>
2021-12-29 15:44:46 +0530

    init

---
+++ lib/src/dashboard/screens/dashboard.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("Dashboard Page"),
+        ),
+      ),
+    );
+  }
+}
